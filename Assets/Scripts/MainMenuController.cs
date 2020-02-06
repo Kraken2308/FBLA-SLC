d9965a209ee328839c6725d4574bbd1d680aebd3
Krish <krishsharma2308@gmail.com>
2020-02-06 07:56:09 -0500

    QualityOfLife

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
---
+++ Assets/Scripts/VerticalPllatform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPllatform : MonoBehaviour
+{
+
+    public PlatformEffector2D effector;
+    public float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            waitTime = 0.5f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.25f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                effector.rotationalOffset = 0f;
+            }
+        }
+    }
+}
